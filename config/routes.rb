8db7d0fcc57bd3af19a2c10d9190e7f0a539ffd7
MICHAEL <milaske@gmail.com>
2015-07-29 14:54:32 -0500

    rock_paper_scissors_homework_solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # This route is for the bare domain. You can ignore it.
-  get("/", { :controller => "game", :action => "home" })
+  #get("/", { :controller => "game", :action => "new_game" })
 
   # Your code goes below.
+
+   get("/",                 { :controller => "game", :action => "move" })
+   get("/move/:move",                 { :controller => "game", :action => "move" })
 
 end
